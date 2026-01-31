b6583ad92ac6a92f75d14cd2f1241a9818e8f871
Klassy01 <davidbeniel2006@gmail.com>
2026-02-01 03:22:39 +0530

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// React import not required with new JSX transform
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
